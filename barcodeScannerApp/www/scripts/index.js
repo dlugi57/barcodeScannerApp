2d36b38004d4dce65408674fef77d6d805817652
Peter Dlugosz <dlugoszpeter@gmail.com>
2018-05-15 16:54:34 +0200

    new app

--- barcodeScannerApp/www/scripts/index.js
+++ barcodeScannerApp/www/scripts/index.js
@@ -1,30 +1,30 @@
-﻿// For an introduction to the Blank template, see the following documentation:
-// http://go.microsoft.com/fwlink/?LinkID=397704
-// To debug code on page load in cordova-simulate or on Android devices/emulators: launch your app, set breakpoints, 
-// and then run "window.location.reload()" in the JavaScript Console.
-(function () {
-    "use strict";
+﻿$("#scanner").click(function () {
 
-    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+    cordova.plugins.barcodeScanner.scan(
+        function (result) {
 
-    function onDeviceReady() {
-        // Handle the Cordova pause and resume events
-        document.addEventListener( 'pause', onPause.bind( this ), false );
-        document.addEventListener( 'resume', onResume.bind( this ), false );
-        
-        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
-        var parentElement = document.getElementById('deviceready');
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-    };
+            $("#search").val(result.text);
 
-    function onPause() {
-        // TODO: This application has been suspended. Save application state here.
-    };
+        },
+        function (error) {
 
-    function onResume() {
-        // TODO: This application has been reactivated. Restore application state here.
-    };
-} )();
+            $("#autocomplete-input").val("Scanning failed: " + error);
+
+        },
+
+    );
+
+});
+
+$("#browser").click(function () {
+
+    window.open($("#search").val(), '_blank');
+
+});
+
+$("#google").click(function () {
+
+    var link = 'https://www.google.fr/search?q=' + $("#search").val();
+    window.open(link, '_blank');
+
+});
